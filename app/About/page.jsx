d3290cb708e3448c97379ed9b80c272ff9308d42
Nedim <mr.ramicevic@gmail.com>
2023-08-01 23:19:20 +0300

    asdas

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className='flex flex-col gap-7 text-center'>
             <div className='text-4xl font-bold'>Biography</div>
             <div className='text-xl'>
-                I am a software engineer with 5+ years of experience in the industry. I have worked on a wide range of projects, from small scale web applications to large scale enterprise applications. I am currently working as a software engineer at <a href='https://www.ing.com.tr/en' className='text-blue-500 hover:text-blue-700'>ING Bank Turkey</a>. I am also a part-time lecturer at <a href='https://www.bilgi.edu.tr/en/' className='text-blue-500 hover:text-blue-700'>Istanbul Bilgi University</a> where I teach web development and software engineering.
+                I am a software engineer with many+ years of experience in the industry. I have worked on a wide range of projects, from small scale web applications to large scale enterprise applications. I am currently working as an engineer at <a href='https://www.turkishairlines.com/' className='text-blue-500 hover:text-blue-700'>turkish Airlines</a>. I am working on React, Nextjs, JavaScript, Tailwind. I am also interested in UI/UX design and I am currently learning Figma.
             </div>
             </div>
             
