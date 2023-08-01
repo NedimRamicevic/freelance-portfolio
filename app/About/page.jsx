7517f9779f25110cd745a425c011c0333e3c2269
Nedim <mr.ramicevic@gmail.com>
2023-08-01 23:21:54 +0300

    s

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className='flex flex-col gap-7 text-center'>
             <div className='text-4xl font-bold'>Biography</div>
             <div className='text-xl'>
-                I am a software engineer with many+ years of experience in the industry. I have worked on a wide range of projects, from small scale web applications to large scale enterprise applications. I am currently working as an engineer at <a href='https://www.turkishairlines.com/' className='text-blue-500 hover:text-blue-700'>turkish Airlines</a>. I am working on React, Nextjs, JavaScript, Tailwind. I am also interested in UI/UX design and I am currently learning Figma.
+                I am a software engineer with many+ years of experience in the industry. I have worked on a wide range of projects, from small scale web applications to large scale enterprise applications. I am currently working as an engineer at <a href='https://www.turkishairlines.com/' className='text-blue-500 hover:text-blue-700'>Turkish Airlines</a>. I am working on React, Nextjs, JavaScript, Tailwind. I am also interested in UI/UX design and I am currently learning Figma.
             </div>
             </div>
             
